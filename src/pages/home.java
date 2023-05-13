6db88d067b26d5f7988f0b12b0d63a82490ce965
Mahardicka <mahardicka404@gmail.com>
2023-05-13 10:07:37 +0700

    vendor preview

--- src/pages/home.java
+++ src/pages/home.java
@@ -147,6 +147,11 @@ public class home extends javax.swing.JFrame {
         jMenu1.add(jMenuItem2);
 
         jMenuItem4.setText("Vendor");
+        jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem4ActionPerformed(evt);
+            }
+        });
         jMenu1.add(jMenuItem4);
 
         jMenuItem3.setText("Barang");
@@ -206,6 +211,11 @@ public class home extends javax.swing.JFrame {
         karyawanPage.onLoad(roleKar.getText());
         karyawanPage.setVisible(true);
     }//GEN-LAST:event_jMenuItem5ActionPerformed
+
+    private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
+        // TODO add your handling code here:
+        vendorPage.setVisible(true);
+    }//GEN-LAST:event_jMenuItem4ActionPerformed
 
     /**
      * @param args the command line arguments
